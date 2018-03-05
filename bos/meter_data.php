92af0ac06bbb128533fbbc4708cf767c7d6e468d
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-03-05 13:58:05 -0500

    taken from https://github.com/EnvironmentalDashboard/machine-learning

---
+++ bos/meter_data.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'class.bos.php';
+ignore_user_abort(1);
+set_time_limit(0);
+$bos = new BuildingOS($db);
+$res = 'quarterhour';
+$chunk = 259200; // download data in week chunks
+foreach ($db->query('SELECT id, url FROM meters ORDER BY RAND()') as $meter) {
+	$bos->updateMeter($meter['id'], $meter['url'] . '/data', $res, $chunk);
+	sleep(3); // don't bombard bos api
+}
+?>
