0d6832e8c75a10a588056120ac6850812b687462
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-03-12 13:41:41 -0400

    collect data for these meters specifically

--- bos/meter_data.php
+++ bos/meter_data.php
@@ -5,7 +5,8 @@ set_time_limit(0);
 $bos = new BuildingOS($db);
 $res = 'quarterhour';
 $chunk = 259200; // download data in week chunks
-foreach ($db->query('SELECT id, url FROM meters ORDER BY RAND()') as $meter) {
+// 578 = cox, 545 = burton, (565, 566, 567, 568, 569, 570) = union houses, 547 = farichild, 588 = firelands, 553 = north, 555 = south, 362 = east
+foreach ($db->query('SELECT id, url FROM meters WHERE building_id IN (578, 545, 565, 566, 567, 568, 569, 570, 547, 588, 553, 555, 362) AND resource = "Water" AND scope = "Whole building"') as $meter) {
 	$bos->updateMeter($meter['id'], $meter['url'] . '/data', $res, $chunk);
 	sleep(3); // don't bombard bos api
 }
