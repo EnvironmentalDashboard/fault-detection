fd2e75dcc36f4b71c89f4ad510e40568ce1587b0
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-03-26 13:31:45 -0400

    testing

--- knn.py
+++ knn.py
@@ -1,10 +1,12 @@
 import pymysql.cursors
 import config
-def main():
-    db = pymysql.connect(host="159.89.232.129", port=3306, user=config.username, password=config.password, db="envs356", autocommit=True)
-    cur = db.cursor()
 
-    cur.execute("SELECT id FROM meters")
-    for meters in cur.fetchall():
-        cur.execute("SELECT value FROM meter_data WHERE meter_id = 316 AND resolution = 'hour' ORDER BY recorded DESC",
-                    (int(meter[0]), res))
+
+def main():
+  db = pymysql.connect(host="159.89.232.129", port=3306, user=config.username, password=config.password, db="envs356", autocommit=True)
+  cur = db.cursor()
+  cur.execute("SELECT id FROM meters")
+  for meter in cur.fetchall():
+    print(meter[0])
+
+main()
