b5cb34de1ce086da2726869eae80eeeb32245185
Muci Yu <muciyu@ip-121-14.wireless.oberlin.edu>
2018-03-26 13:26:25 -0400

    add

--- knn.py
+++ knn.py
@@ -1,6 +1,10 @@
 import pymysql.cursors
-import config 
-db = pymysql.connect(host="159.89.232.129", port=3306, user=config.username, password=config.password, db="envs356", autocommit=True)
-cur = db.cursor()
+import config
+def main():
+    db = pymysql.connect(host="159.89.232.129", port=3306, user=config.username, password=config.password, db="envs356", autocommit=True)
+    cur = db.cursor()
 
-cur.execute("SELECT id FROM meters")
+    cur.execute("SELECT id FROM meters")
+    for meters in cur.fetchall():
+        cur.execute("SELECT value FROM meter_data WHERE meter_id = 316 AND resolution = 'hour' ORDER BY recorded DESC",
+                    (int(meter[0]), res))
