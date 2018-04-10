18bb9f99a60b9f760412cf3282d6fbe7983d35b2
Muci Yu <muciyu@Mucis-MacBook-Pro.local>
2018-04-10 16:13:29 -0400

    test2

--- test2.py
+++ test2.py
@@ -15,12 +15,12 @@ from sklearn.cluster import KMeans
 from sklearn.covariance import EllipticEnvelope
 from sklearn.ensemble import IsolationForest
 from sklearn.svm import OneClassSVM
-from pyemma import msm
+#from pyemma import msm
 
 
 def main():
     engine = create_engine(config.sqlalchemy_uri)
-    sql = "SELECT value, recorded FROM meter_data WHERE meter_id = 343 AND resolution = 'quarterhour' AND value IS NOT NULL ORDER BY recorded ASC"
+    sql = "SELECT value, recorded FROM meter_data WHERE meter_id = 464 AND resolution = 'quarterhour' AND value IS NOT NULL ORDER BY recorded ASC"
     df = pd.read_sql_query(sql, engine)
     df['recorded'] = pd.to_datetime(df['recorded'], unit='s')
     df['hours'] = df['recorded'].dt.hour
@@ -34,30 +34,49 @@ def main():
     # creation of 4 distinct categories that seem useful (week end/day week & night/day)
     df['categories'] = df['WeekDay'] * 2 + df['daylight']
 
-    a = df.loc[df['categories'] == 0, 'value']
-    b = df.loc[df['categories'] == 1, 'value']
-    c = df.loc[df['categories'] == 2, 'value']
-    d = df.loc[df['categories'] == 3, 'value']
-
-    fig, ax = plt.subplots()
-    a_heights, a_bins = np.histogram(a)
-    b_heights, b_bins = np.histogram(b, bins=a_bins)
-    c_heights, c_bins = np.histogram(c, bins=a_bins)
-    d_heights, d_bins = np.histogram(d, bins=a_bins)
-
-    width = (a_bins[1] - a_bins[0]) / 6
-
-    ax.bar(a_bins[:-1], a_heights * 100 / a.count(), width=width, facecolor='blue', label='WeekEndNight')
-    ax.bar(b_bins[:-1] + width, (b_heights * 100 / b.count()), width=width, facecolor='green', label='WeekEndLight')
-    ax.bar(c_bins[:-1] + width * 2, (c_heights * 100 / c.count()), width=width, facecolor='red', label='WeekDayNight')
-    ax.bar(d_bins[:-1] + width * 3, (d_heights * 100 / d.count()), width=width, facecolor='black', label='WeekDayLight')
-
-    plt.legend()
-    plt.show()
+    print(df.value.describe())
+    print("===============pre standardized============")
     print(df.head())
-    print(df.info())
-    # print(df.loc['value' == 0])
+    data = standardize(df)
+    print("===============post standardized============")
+    print(data.head())
+    print(data.value.describe())
 
+    qlist = defineStates(data)
+    df_mm = markovStates(df,qlist)
+
+    # getting the anomaly labels for our dataset (evaluating sequence of 5 values and anomaly = less than 20% probable)
+    df_anomaly = markovAnomaly(df_mm, 5, 0.20)
+    df_anomaly = pd.Series(df_anomaly)
+    print(df_anomaly.value_counts())
+
+def standardize(df):
+    data = df[['value', 'hours', 'daylight', 'DayOfTheWeek', 'WeekDay']]
+    min_max_scaler = preprocessing.StandardScaler()
+    np_scaled = min_max_scaler.fit_transform(data[['value']])
+    temp_df = pd.DataFrame(np_scaled)
+    data['value'] = temp_df[0]
+    return data
+
+
+def defineStates(df):
+    q2 = df.value.quantile(0.2)
+    q4 = df.value.quantile(0.4)
+    q6 = df.value.quantile(0.6)
+    q8 = df.value.quantile(0.8)
+    return q2,q4,q6,q8
+
+
+def markovStates(df, qlist):
+    # definition of the different state
+    print(qlist)
+    x1 = (df['value'] <= qlist[0]).astype(int)
+    x2 = ((df['value'] > qlist[0]) & (df['value'] <= qlist[1])).astype(int)
+    x3 = ((df['value'] > qlist[1]) & (df['value'] <= qlist[2])).astype(int)
+    x4 = ((df['value'] > qlist[2]) & (df['value'] <= qlist[3])).astype(int)
+    x5 = (df['value'] > qlist[3]).astype(int)
+    df_mm = x1 + 2 * x2 + 3 * x3 + 4 * x4 + 5 * x5
+    return df_mm
 
 def markovAnomaly(df, windows_size, threshold):
     transition_matrix = getTransitionMatrix(df)
